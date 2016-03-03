5c52d9ad312225b38e05d6f855327c37bdecde85
Jacob Dorpinghaus <jdorp@charter.net>
2016-03-03 02:48:08 +0000

    Added button links, navbutton clicks, setbullet function

--- js/site.js
+++ js/site.js
@@ -1,3 +1,39 @@
+var projects=5;
+var project=1;
 $(document).ready(function(){
+    addGalleryClicks();
+    setBullet();
     console.log('loaded');
 });
+
+function addGalleryClicks(){
+    $("#leftButton i").click(function(){
+        console.log('left clicked');
+        if (project==1){
+            project=5;
+        } else {
+            project--;
+        }
+        setBullet();
+    });
+    $("#rightButton i").click(function(){
+        if (project==projects){
+            project=1;
+        } else {
+            project++;
+        }
+        console.log('right clicked');
+        setBullet();
+    });
+}
+function setBullet(){
+    var i;
+    for(i=0;i<projects;i++){
+        $("#bullet_" + (i+1).toString()).css("background", "rgba(255,255,255, .5)");
+    }
+    $("#bullet_" + project.toString()).css("background", "rgba(255,255,255, 1)");
+}
+function setProject(){
+    var imgurl = "css/img/projectsplash_" + project.toString();
+    $("#background").attr("background", "url("+imgurl+")");
+}
