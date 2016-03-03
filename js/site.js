7dc9cbd40e1bbdded35f588380dc66a961ebdcf9
Jacob Dorpinghaus <jdorp@charter.net>
2016-03-03 01:46:58 +0000

    Added link buttons

--- js/site.js
+++ js/site.js
@@ -1,10 +1,3 @@
-/*global angular*/
-
-var app = angular.module('app', []);
-app.controller('ctrl', function($scope) {
-    $scope.test="test";
-});
-
 $(document).ready(function(){
     console.log('loaded');
 });
